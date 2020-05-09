ae4c4330bcc7860b1de91bcbcfaac943e73b180c
Jad Haddad <jad.haddad@zto-technology.com>
2020-05-09 23:29:10 +0200

    Add dataset module

---
+++ covid-mask-detector/dataset.py
@@ -0,0 +1,33 @@
+""" Dataset module
+"""
+from torch import long, tensor
+from torch.utils.data.dataset import Dataset
+from torchvision.transforms import Compose, Resize, ToPILImage, ToTensor
+
+
+class MaskDataset(Dataset):
+    """ Masked faces dataset
+        0 = 'no mask'
+        1 = 'mask'
+    """
+    def __init__(self, dataFrame):
+        self.dataFrame = dataFrame
+        
+        self.transformations = Compose([
+            ToPILImage(),
+            Resize((100, 100)),
+            ToTensor(), # [0, 1]
+        ])
+    
+    def __getitem__(self, key):
+        if isinstance(key, slice):
+            raise NotImplementedError('slicing is not supported')
+        
+        row = self.dataFrame.iloc[key]
+        return {
+            'image': self.transformations(row['image']),
+            'mask': tensor([row['mask']], dtype=long), # pylint: disable=not-callable
+        }
+    
+    def __len__(self):
+        return len(self.dataFrame.index)
